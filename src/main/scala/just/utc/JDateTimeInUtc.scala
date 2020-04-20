9dee5aabbaccbb75b1d5809b8b0dbad270a978f7
Kevin Lee <kevin.code@kevinlee.io>
2020-04-20 22:49:07 +1000

    Changed: ZoneId for UTC from ZoneId.ofOffset("UTC", ZoneOffset.UTC) to just ZoneOffset.UTC

--- src/main/scala/just/utc/JDateTimeInUtc.scala
+++ src/main/scala/just/utc/JDateTimeInUtc.scala
@@ -7,10 +7,11 @@ import java.time.{Instant, LocalDateTime, ZoneId, ZoneOffset}
   * @since 2018-09-12
   */
 object JDateTimeInUtc {
-  val zoneId: ZoneId = ZoneId.ofOffset("UTC", ZoneOffset.UTC)
+  final val ZoneOffsetUtc: ZoneOffset = ZoneOffset.UTC
+  final val ZoneIdUtc: ZoneId = ZoneOffsetUtc
 
   def toLocalDateTime(millis: Long): LocalDateTime =
-    LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), zoneId)
+    LocalDateTime.ofInstant(Instant.ofEpochMilli(millis), ZoneIdUtc)
 
   def toEpochMilli(localDateTime: LocalDateTime): Long =
     localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli
